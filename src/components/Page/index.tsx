119ecacbe8a370dce9316532363a685d404f9fcd
Victor Campos <campos.viictor@gmail.com>
2020-10-26 19:09:33 -0300

    creating page and default overlay content

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { ModelsWrapper, ModelSection } from '../Model';
+import DefaultOverlayContent from '../DefaultOverlayContent';
+import UniqueOverlay from '../UniqueOverlay';
+
+import { Container } from './styles';
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+      <ModelsWrapper>
+        <div>
+          {[
+            {name:'Model 3', description: 'Order online for Touchless Delivery'},
+            {name:'Model S', description: 'Order online for Touchless Delivery'},
+            {name:'Model X', description: 'Order online for Touchless Delivery'},
+            {name:'Model Y', description: 'Order online for Touchless Delivery'},
+            {name:'Model A', description: 'Order online for Touchless Delivery'},
+            {name:'Model B', description: 'Order online for Touchless Delivery'},
+            {name:'Model 9', description: 'Order online for Touchless Delivery'}
+          ].map(model => (
+            <ModelSection
+              key={model.name} 
+              className="colored"
+              modelName={model.name}
+              overlayNode={
+                <DefaultOverlayContent 
+                  label={model.name}
+                  description={model.description}
+                />
+              }
+            /> 
+          ))}
+        </div>
+
+        <UniqueOverlay />
+      </ModelsWrapper>
+    </Container>
+  );
+};
+
+export default Page;
---
+++ src/components/Page/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  .colored:nth-child(1) {
+    background: url('https://cdn.motor1.com/images/mgl/b2xGj/s1/tesla-model-3.jpg') no-repeat;
+    background-size: cover; 
+    background-position: center center;
+  }
+  .colored:nth-child(2) {
+    background: url('https://wallpapercave.com/wp/wp1821219.jpg') no-repeat;
+    background-size: cover; 
+    background-position: center center;
+  }
+  .colored:nth-child(3) {
+    background: url('https://cdn.motor1.com/images/mgl/qQ3ng/s3/tesla-model-x-wallpaper-2-560x.jpg') no-repeat;
+    background-size: cover; 
+    background-position: center center;
+  }
+  .colored:nth-child(4) {
+    background: url('https://cdn.motor1.com/images/mgl/VpmYz/s1/tesla-model-y.jpg') no-repeat;
+    background-size: cover; 
+    background-position: center center;
+  }
+  .colored:nth-child(5) {
+  background: #a9fdac;
+  }
+  .colored:nth-child(6) {
+  background: #90f29c;
+  }
+  .colored:nth-child(7) {
+  background: #77e68c;
+  }
+`;
